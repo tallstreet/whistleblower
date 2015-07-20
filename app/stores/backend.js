dfcd126809714667b68269500400fc8118e5ac38
Gary Roberts <gary.roberts@smartfocus.com>
2015-07-20 16:00:19 +0100

    Add extra state to backend store

--- app/stores/backend.js
+++ app/stores/backend.js
@@ -1,4 +1,4 @@
-import { POST_SUCCESS, POST_FAILURE, TIMER_COMPLETE } from '../constants/ActionTypes';
+import { POST_SUCCESS, POST_FAILURE, TIMER_COMPLETE, CHECK_SUCCESS, CHECK_FAILURE } from '../constants/ActionTypes';
 import { redux } from '../containers/App';
 import { startTimer } from '../actions/TimerActions';
 import { check } from '../actions/BackendActions';
@@ -9,9 +9,13 @@ export default function backend(state = {}, action) {
   case POST_SUCCESS:
     redux.dispatch(startTimer(action.account, action.amount));
     return action.result;
+  case CHECK_SUCCESS:
+    return action.result;
   case TIMER_COMPLETE:
     redux.dispatch(check(action.account));
     return state;
+  case CHECK_FAILURE:
+    return action.error;
   case POST_FAILURE:
     return action.error;
   default:
